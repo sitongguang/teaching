6008eb2ceae5debf0333e101812bb5c7c668e419
stone <stg@sdjzu.edu.cn>
2019-12-05 16:30:26 +0800

    加入session

--- cat/src/main/java/controller/security/LoginController.java
+++ cat/src/main/java/controller/security/LoginController.java
@@ -17,20 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
     private static final Logger logger = LogManager.getLogger();
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-
-
-
-        logger.info("doPost");
-        //设置响应字符编码为UTF-8
-        response.setContentType("text/html;charset=UTF-8");
         //创建JSON对象message，以便往前端响应信息
         JSONObject message = new JSONObject();
         try {
@@ -38,9 +33,11 @@ public class LoginController extends HttpServlet {
             if(loggedUser!=null){
                 message.put("message", "登录成功");
                 HttpSession session = request.getSession();
-                session.setAttribute("user" , loggedUser);
+                //10分钟没有操作，则使session失效
+                session.setMaxInactiveInterval(10 * 60);
+                session.setAttribute("currentUser" , loggedUser);
                 response.getWriter().println(message);
-//                response.getWriter().println(JSON.toJSONString(loggedUser));
+                //此处应重定向到索引页（菜单页）
                 return;
             }else{
                 message.put("message", "用户名或密码错误");
