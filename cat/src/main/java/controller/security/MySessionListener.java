b00822aeb845e5f7f034fe04580dc6d0e7115046
stone <stg@sdjzu.edu.cn>
2019-12-07 14:08:30 +0800

    打印sessionId

--- cat/src/main/java/controller/security/MySessionListener.java
+++ cat/src/main/java/controller/security/MySessionListener.java
@@ -4,14 +4,18 @@ import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 @WebListener
-public class SessionListener implements HttpSessionListener {
+public class MySessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        System.out.println("session created");
+        System.out.println("the newly created session's id is ");
+        System.out.print(se.getSession().getId());
+
+
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        System.out.println("session destroyed");
+        System.out.println("the newly destroyed session's id is ");
+        System.out.print(se.getSession().getId());
     }
 }
